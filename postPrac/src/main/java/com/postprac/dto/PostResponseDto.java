2d061a82eee2b73efe99c19f96cbfac73cdfe2d8
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 13:29:26 +0900

    feat: 반환 형태 추가

--- postPrac/src/main/java/com/postprac/dto/PostResponseDto.java
+++ postPrac/src/main/java/com/postprac/dto/PostResponseDto.java
@@ -1,10 +1,24 @@
 package com.postprac.dto;
 
+import com.postprac.entity.Post;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PostResponseDto {
     private String username;
     private String title;
     private String content;
+
+    public PostResponseDto(PostRequestDto requestDto){
+        this.username = requestDto.getUsername();
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+    }
+    public PostResponseDto(Post post){
+        this.username = post.getUsername();
+        this.title = post.getTitle();
+        this.content= post.getContent();
+    }
 }
