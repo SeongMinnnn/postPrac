c771e7d32e27b0973b3c81df7d637c5e4169e272
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 12:47:36 +0900

    feat: Post 생성

--- postPrac/src/main/java/com/postprac/entity/Post.java
+++ postPrac/src/main/java/com/postprac/entity/Post.java
@@ -1,4 +1,17 @@
 package com.postprac.entity;
 
+import com.postprac.dto.PostRequestDto;
+import lombok.Getter;
+
+@Getter
 public class Post {
+    private String username;
+    private String title;
+    private String content;
+
+    public Post(PostRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+    }
 }
