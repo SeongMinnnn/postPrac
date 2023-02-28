52eb4e19f2beb7d9ca46e97a214457087581c576
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 13:02:51 +0900

    feat: PostService생성

--- postPrac/src/main/java/com/postprac/service/PostService.java
+++ postPrac/src/main/java/com/postprac/service/PostService.java
@@ -1,4 +1,36 @@
 package com.postprac.service;
 
+import com.postprac.dto.PostRequestDto;
+import com.postprac.dto.PostResponseDto;
+import com.postprac.entity.Post;
+import com.postprac.repository.PostRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+@RequiredArgsConstructor
 public class PostService {
+    private final Post post;
+    private final PostRepository postRepository;
+    public PostResponseDto create(PostRequestDto requestDto) {
+        postRepository.create(requestDto);
+        return PostResponseDto(requestDto);
+    }
+
+    public PostResponseDto read(Long id) {
+        Post post = postRepository.findById(id);
+        return PostResponseDto(post);
+    }
+
+    public PostResponseDto update(PostRequestDto requestDto) {
+        post.update(requestDto);
+        return PostResponseDto(requestDto);
+    }
+
+    public ResponseDto<String> create(PostRequestDto requestDto) {
+        postRepository.delete(requestDto);
+        return ResponseDto(200,"삭제완료");
+    }
 }
