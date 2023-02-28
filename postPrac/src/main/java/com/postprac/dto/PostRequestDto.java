773cf70c299157a9fbfc230ce29ffded9af645cb
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 12:48:40 +0900

    feat: PostRequestDto

--- postPrac/src/main/java/com/postprac/dto/PostRequestDto.java
+++ postPrac/src/main/java/com/postprac/dto/PostRequestDto.java
@@ -1,4 +1,12 @@
 package com.postprac.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class PostRequestDto {
+    private String username;
+    private String title;
+    private String content;
 }
