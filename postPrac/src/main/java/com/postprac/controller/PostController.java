2a39f3e4f239ab6a3463d346b1b0808180554850
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 12:55:39 +0900

    feat: PostController 생성

--- postPrac/src/main/java/com/postprac/controller/PostController.java
+++ postPrac/src/main/java/com/postprac/controller/PostController.java
@@ -1,4 +1,33 @@
 package com.postprac.controller;
 
+import com.postprac.dto.PostRequestDto;
+import com.postprac.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class PostController {
+    private final PostService postService;
+
+    @PostMapping("/post")
+    public ResponseDto<?> create(@RequestBody PostRequestDto requestDto){
+        return postService.create(requestDto);
+    }
+
+    @GetMapping("/post")
+    public ResponseDto<?> read(@PathVariable Long id){
+        return postService.read(id);
+    }
+
+    @PutMapping("/post")
+    public ResponseDto<?> update(@RequestBody PostRequestDto requestDto){
+        return postService.update(requestDto);
+    }
+
+    @GetMapping("/post")
+    public ResponseDto<?> delete(@PathVariable Long id){
+        return postService.delete(id);
+    }
 }
