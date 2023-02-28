2ea338b1199328bf128e77d07fc469985d13a5e7
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 12:49:55 +0900

    feat: PostResponseDto 생성

--- postPrac/src/main/java/com/postprac/dto/PostResponseDto.java
+++ postPrac/src/main/java/com/postprac/dto/PostResponseDto.java
@@ -1,4 +1,10 @@
 package com.postprac.dto;
 
+import lombok.Getter;
+
+@Getter
 public class PostResponseDto {
+    private String username;
+    private String title;
+    private String content;
 }
